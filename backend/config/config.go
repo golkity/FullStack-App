66fcc0c9af88c287e87f54fb9d569db3bb57bd4a
Finnik <73883383+golkity@users.noreply.github.com>
2025-01-28 12:18:46 +0300

    Add: func loadConfig && config.json

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,5 +1,27 @@
 package config
 
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
 type Config struct {
 	Port string `json:"port"`
+}
+
+func LoadConfig(filename string) (*Config, error) {
+	data, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer data.Close()
+
+	byteValue, _ := io.ReadAll(data)
+
+	var config Config
+
+	json.Unmarshal(byteValue, &config)
+
+	return &config, nil
 }
