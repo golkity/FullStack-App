3fb5518685650867aece7a234ddd717029703cbb
Finnik <73883383+golkity@users.noreply.github.com>
2025-01-31 08:22:07 +0300

    Add: request DELET:ID

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -18,8 +18,8 @@ func main() {
 
 	todos := []Todo{}
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(fiber.Map{"msg": "Hello World"})
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(todos)
 	})
 
 	app.Post("/api/todos", func(c *fiber.Ctx) error {
@@ -50,6 +50,20 @@ func main() {
 		}
 
 		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
+	})
+
+	app.Delete("api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos {
+			if fmt.Sprint(todo.ID) == id {
+				todos = append(todos[:i], todos[i+1:]...)
+				return c.Status(200).JSON(fiber.Map{"success": "true"})
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "todo not found"})
+
 	})
 
 	log.Fatal(app.Listen(":8080"))
